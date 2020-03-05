981f8e425d9c772df9b5311e9fefc495347de12c
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-03-04 21:55:52 -0300

    Fix day-1 bugs o.o

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool receivedFixedMarker = false;
 
 tf2::Vector3 referencePosition;
-if2::Quaternion referenceRotation;
+tf2::Quaternion referenceRotation;
 
 tf2::Vector3 sendingPosition;
 tf2::Quaternion sendingRotation;
@@ -18,32 +18,32 @@ void referenceCallback(const geometry_msgs::Pose::ConstPtr& referenceData)
 {
     receivedFixedMarker = true;
 
-    fromMsg(referenceData->Point, referencePosition);
-    fromMsg(referenceData->Quaternion, referenceQuarternion);
+    fromMsg(referenceData->position, referencePosition);
+    fromMsg(referenceData->orientation, referenceRotation);
 }
 
 void inPoseCallback(const geometry_msgs::Pose::ConstPtr& receivedData)
 {
-    if (receveidFixedMarker == false)
+    if (receivedFixedMarker == false)
         return;
 
     tf2::Vector3 temporaryPosition; 
     tf2::Quaternion temporaryRotation;
     
-    fromMsg(receivedData->Point, temporaryPosition);
-    fromMsg(receivedData->Quaternion, temporaryRotation);
+    fromMsg(receivedData->position, temporaryPosition);
+    fromMsg(receivedData->orientation, temporaryRotation);
 
     temporaryPosition = referencePosition - temporaryPosition;
     sendingPosition = quatRotate(referenceRotation, temporaryPosition);
 
     sendingRotation = referenceRotation.inverse() * temporaryRotation;
 
-    geometry_msgs::Point positionMessage = toMsg(sendingPosition, positionMessage&);
+    geometry_msgs::Point positionMessage = toMsg(sendingPosition, positionMessage);
     geometry_msgs::Quaternion quaternionMessage = toMsg(sendingRotation);
 
-    geometry_msg::Pose fullMessage;
-    fullMessage.Point = positionMessage;
-    fullMessage.Quaternion = quaternionMessage;
+    geometry_msgs::Pose fullMessage;
+    fullMessage.position = positionMessage;
+    fullMessage.orientation = quaternionMessage;
 
     publisher.publish(fullMessage);
 }
@@ -53,9 +53,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "camera_to_fixed_frame");
     ros::NodeHandle nodeHandle;
 
-    ros::Subscriber subscriber = nodeHandle.subscribe("reference_pose", 1000, referenceCallback);
+    ros::Subscriber referenceSubscriber = nodeHandle.subscribe("reference_pose", 1000, referenceCallback);
 
-    ros::Subscriber subscriber = nodeHandle.subscribe("in_pose", 1000, inPoseCallback);
+    ros::Subscriber inputSubscriber = nodeHandle.subscribe("in_pose", 1000, inPoseCallback);
 
     publisher = nodeHandle.advertise<geometry_msgs::Pose>("out_pose", 1000);
 
