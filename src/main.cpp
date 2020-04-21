4cf0a185d1fa2e74ff0a1f57f95aaceff2ae8b23
nori12 <phbnori@gmail.com>
2020-04-21 03:58:27 -0300

    tf2 migration done in order to correct rotations with respect to camera fixed frame; node name changed to 'position_estimator'; output type changed to PoseStamped.

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,64 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
 
 using namespace std;
 
-bool receivedFixedMarker = false;
-
-tf2::Vector3 referencePosition;
-tf2::Quaternion referenceRotation;
-
-tf2::Vector3 sendingPosition;
-tf2::Quaternion sendingRotation;
-
 ros::Publisher publisher;
-
-void referenceCallback(const geometry_msgs::PoseStamped::ConstPtr& referenceData)
-{
-    receivedFixedMarker = true;
-
-    fromMsg(referenceData->pose.position, referencePosition);
-    fromMsg(referenceData->pose.orientation, referenceRotation);
-}
-
-void inPoseCallback(const geometry_msgs::PoseStamped::ConstPtr &receivedData)
-{
-    if (receivedFixedMarker == false)
-        return;
-
-    tf2::Vector3 temporaryPosition; 
-    tf2::Quaternion temporaryRotation;
-    
-    fromMsg(receivedData->pose.position, temporaryPosition);
-    fromMsg(receivedData->pose.orientation, temporaryRotation);
-
-    temporaryPosition = referencePosition - temporaryPosition;
-    sendingPosition = quatRotate(referenceRotation, temporaryPosition);
-
-    sendingRotation = referenceRotation.inverse() * temporaryRotation;
-
-    geometry_msgs::Point positionMessage = toMsg(sendingPosition, positionMessage);
-    geometry_msgs::Quaternion quaternionMessage = toMsg(sendingRotation);
-
-    geometry_msgs::Pose fullMessage;
-    fullMessage.position = positionMessage;
-    fullMessage.orientation = quaternionMessage;
-
-    publisher.publish(fullMessage);
-}
+string reference_frame_name;
+string robot_frame_name;
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "camera_to_fixed_frame");
+    ros::init(argc, argv, "position_estimator");
     ros::NodeHandle nodeHandle;
+    ros::NodeHandle node;
 
-    ros::Subscriber referenceSubscriber = nodeHandle.subscribe("reference_pose", 1000, referenceCallback);
+    if (ros::param::get("~reference", reference_frame_name) == false)
+    {
+        ROS_FATAL("Parameter reference not set.");
+        return -1;
+    }
 
-    ros::Subscriber inputSubscriber = nodeHandle.subscribe("in_pose", 1000, inPoseCallback);
+    if (ros::param::get("~robot", robot_frame_name) == false)
+    {
+        ROS_FATAL("Parameter reference not set.");
+        return -1;
+    }    
 
-    publisher = nodeHandle.advertise<geometry_msgs::Pose>("out_pose", 1000);
+    publisher = nodeHandle.advertise<geometry_msgs::PoseStamped>("position", 1000);    
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    ros::Rate rate(10.0);
+    while (node.ok()){
+        geometry_msgs::TransformStamped transformStamped;
+        geometry_msgs::PoseStamped positionTransformed;
+
+        try{
+            transformStamped = tfBuffer.lookupTransform(reference_frame_name, robot_frame_name, ros::Time(0));
+            
+            positionTransformed.header = transformStamped.header;
+            positionTransformed.pose.position.x = transformStamped.transform.translation.x;
+            positionTransformed.pose.position.y = transformStamped.transform.translation.y;
+            positionTransformed.pose.position.z = transformStamped.transform.translation.z;
+            positionTransformed.pose.orientation.x = transformStamped.transform.rotation.x;
+            positionTransformed.pose.orientation.y = transformStamped.transform.rotation.y;
+            positionTransformed.pose.orientation.z = transformStamped.transform.rotation.z;
+            positionTransformed.pose.orientation.w = transformStamped.transform.rotation.w;
+        }
+        catch (tf2::TransformException &ex) {
+            ROS_WARN("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+
+        publisher.publish(positionTransformed);
+        rate.sleep();
+    }
 
     ros::spin();
 
